cedbcc6e8572fa6bfe038c55a9cf543522de7e50
invincibleinventor <invincibleinventor@gmail.com>
2022-11-08 14:35:34 +0530

    ok

--- src/components/registration.js
+++ src/components/registration.js
@@ -3,11 +3,7 @@ import styles from "../forms.css"
 import Select from 'react-select'
 import { initializeApp } from "firebase/app";
 import { getDatabase,ref,set,child,push } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAmZmuckTBZuUwetU3xROn0DBmdFjGZpvE",
   authDomain: "ttstvsschool.firebaseapp.com",
@@ -19,7 +15,6 @@ const firebaseConfig = {
   measurementId: "G-H56FQJJJ16"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
