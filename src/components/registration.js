c356764332d983be2af00f2d5995be1f1378ccee
invincibleinventor <invincibleinventor@gmail.com>
2022-11-08 11:42:13 +0530

    ok

--- src/components/registration.js
+++ src/components/registration.js
@@ -1,6 +1,34 @@
 import React from 'react'
 import styles from "../forms.css"
 
+function form(){
+  return(
+    <><div class="background">
+      <div class="shape"></div>
+      <div class="shape"></div>
+    </div><form>
+        <h3>Register</h3>
+
+        <label for="username">Name</label>
+        <input type="text" placeholder="Your Name" id="username" />
+
+        <label for="schl">School Name</label>
+        <input type="text" placeholder="School Name" id="schl" />
+        <label for="ph">Phone Number</label>
+        <input type="number" placeholder="Phone Number" id="ph" />
+
+        <label for="wa">WhatsApp Number</label>
+        <input type="number" placeholder="WhatsApp Number" id="wa" />
+
+        <label for="class">Class</label>
+        <input type="number" placeholder="Class" id="class" />
+
+
+        <button>Register</button>
+
+      </form></>
+  )
+}
 function Register() {
 
 return (
@@ -9,31 +37,7 @@ return (
     width: '100vw',
     height: '100vh'
   }}>
-    <div class="background">
-    <div class="shape"></div>
-    <div class="shape"></div>
-</div>
-<form>
-    <h3 >Register</h3>
-
-    <label for="username">Name</label>
-    <input type="text" placeholder="Your Name" id="username" />
-
-    <label for="schl">School Name</label>
-    <input type="text" placeholder="School Name" id="schl" />
-    <label for="ph">Phone Number</label>
-    <input type="number" placeholder="Phone Number" id="ph" />
-
-    <label for="wa">WhatsApp Number</label>
-    <input type="number" placeholder="WhatsApp Number" id="wa" />
-
-    <label for="class">Class</label>
-    <input type="number" placeholder="Class" id="class" />
-
-
-    <button>Register</button>
    
-</form>
 </div>
   )
 }
